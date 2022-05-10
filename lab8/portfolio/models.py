369d7d9261426f7a9fd36b8bcf8436a502f2f8d7
a22000492 <ricardo_a_goncalves@hotmail.com>
2022-05-11 00:07:00 +0100

    lab9 1

--- lab8/portfolio/models.py
+++ lab8/portfolio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    autor = models.CharField(max_length=100)
+    titulo = models.CharField(max_length=200)
+    descricao = models.CharField(max_length=500)
+    criado = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo[:50]
