e2656e0b34c6961fa67009404f7ad3745c3cd54d
a22000492 <ricardo_a_goncalves@hotmail.com>
2022-05-11 23:36:27 +0100

    lab9 - fix home link

--- lab8/portfolio/urls.py
+++ lab8/portfolio/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "portfolio"
 
 urlpatterns = [
-    path('home', views.home_page_view, name='home'),
+    path('', views.home_page_view, name='home'),
     path('apresentacao', views.apresentacao_page_view, name='apresentacao'),
     path('competencias', views.competencias_page_view, name='competencias'),
     path('formacao', views.formacao_page_view, name='formacao'),
